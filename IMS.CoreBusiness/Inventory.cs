d99b6f10b71edcdee10cb1e3bd4ad0b7890ebd27
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-04-24 19:33:44 +0200

    Rename and some fix

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -3,7 +3,7 @@
     public class Inventory
     {
         public int InventoryId { get; set; }
-        public string InventoryName{ get; set; }
+        public string InventoryName{ get; set; } = string.Empty;
         public int Quantity { get; set; }
         public double Price { get; set; }
     }
