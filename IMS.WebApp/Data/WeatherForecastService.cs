d9185f925568f5956084c40271d7c9207d60e4c0
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-05-19 22:30:13 +0200

    Chart и ссылка на техподдержку

--- IMS.WebApp/Data/WeatherForecastService.cs
+++ IMS.WebApp/Data/WeatherForecastService.cs
@@ -9,7 +9,7 @@ namespace IMS.WebApp.Data
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(1, 25).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
