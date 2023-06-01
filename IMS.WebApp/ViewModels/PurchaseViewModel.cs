0d7380738437797713aabaa0763543304a78376c
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-06-01 10:41:19 +0200

    Страница оформления заказа комплектующих

---
+++ IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PurchaseOrder { get; set; }
+        [Required]
+        public int InventoryId { get; set; }
+        [Required]
+        public string InventoryName { get; set; }
+        [Required]
+        [Range(minimum:1, maximum: int.MaxValue, ErrorMessage = "Минимальное количество для оформления заказа: 1")]
+        public int QuantityToPurchase { get; set; }
+    }
+}
