4eed1d02b8aaf3503c994ca5291a9fa694f3c82a
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-06-01 10:46:29 +0200

    Purchase  use case

---
+++ IMS.UseCases/Purchases/PurchaseInventoryUseCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Purchases
+{
+    public class PurchaseInventoryUseCase
+    {
+        public async Task ExecuteAsync(string poNumber, int inventoryId, int quantity)
+        {
+
+        }
+    }
+}
