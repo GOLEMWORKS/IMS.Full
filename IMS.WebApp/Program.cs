879398f4aeed43ccb2ef40f3fccf04284dfdfb37
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-06-05 10:42:45 +0200

    Фикс для  докера на ноуте

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -81,7 +81,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
