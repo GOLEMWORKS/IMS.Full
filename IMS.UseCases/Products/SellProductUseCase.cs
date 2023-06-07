dbc947545f5e4c3926dd7b8beaba2655717e8994
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-06-07 21:37:05 +0200

    8.16.43 SellProductsUseCase

---
+++ IMS.UseCases/Products/SellProductUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Products
+{
+    public class SellProductUseCase
+    {
+        //8.16.43
+    }
+}
