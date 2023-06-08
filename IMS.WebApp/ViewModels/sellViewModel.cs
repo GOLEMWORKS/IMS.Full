ac5318d6b86b1e22ac811dc756a4579226668c6d
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-06-08 11:45:11 +0200

    8.30.28

---
+++ IMS.WebApp/ViewModels/sellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class SellViewModel
+    {
+        [Required]
+        public string SalesOrderNumber { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Минимальное количество для продажи: 1")]
+        public int QuantityToSell { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
