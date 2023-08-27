82b44bf02d2d058c3a84a5b8f6b23831ac9f4eb4
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-28 01:25:57 +0200

    work fin 28/08/23

--- IMS.WebApp/Data/Migrations/20230827231916_barcodeString.Designer.cs
+++ IMS.WebApp/Data/Migrations/20230827231916_barcodeString.Designer.cs
@@ -4,6 +4,7 @@ using IMS.WebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IMS.WebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230827231916_barcodeString")]
+    partial class barcodeString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IMS.WebApp/Data/Migrations/20230827231916_barcodeString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.WebApp.Data.Migrations
+{
+    public partial class barcodeString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
